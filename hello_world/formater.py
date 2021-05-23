3b920ab263fbe70231599f0012f83450cc76e73a
Maciek Kokot <mk@b-sharp.pl>
2021-05-23 13:36:43 +0200

    implement XML handling

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -4,8 +4,9 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
 
 def get_formatted(msg, imie, format):
@@ -18,7 +19,15 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
+
+
+def format_to_xml(msg, imie):
+    xml_output = \
+        f'<greetings>\n<name>{imie}</name>\n<msg>{msg}</msg>\n</greetings>'
+    return xml_output
 
 
 def format_to_json(msg, imie):
